3ca14d64eb1a9f4ab6dba88eab7ffe60e257d23e
Gabriela <gabliquet@gmail.com>
2021-07-17 22:22:02 -0300

    agregue pruebas

--- ulises/ulises.py
+++ ulises/ulises.py
@@ -2,10 +2,15 @@ from bot_base.bot import connectBot, onPrivmessage, onPubmessage
 
 def recibiMensajePrivado(nick, mensaje):
     print(nick, mensaje)
+    if nick == "Gabriela-":
+        return "hola"
+    elif nick == "Alex":
+        return "adios"
 
 def recibiPingCanal(nick, mensaje, canal):
     print(nick, mensaje, canal)
-
+    if mensaje == "hola":
+        return "escribe algo o te mato"
 def run_ulises(): 
     bot = connectBot("irc.hirana.net", 6667, "#BarmanTest", "Ulises")
     onPrivmessage(bot, recibiMensajePrivado)
